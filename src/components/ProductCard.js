3e7643a619567c1ae86b9d0b2fd1d6b55b4909aa
NAYAB SYED DOULATH BASHA <96289292+doulath123@users.noreply.github.com>
2023-02-07 15:35:57 +0530

    Our Store page updated

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactStars from 'react-rating-stars-component';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const ProductCard = () => {
-  return <div className='col-3'>
+const ProductCard = (props) => {
+    const {grid}=props;
+    let location=useLocation();
+    
+
+  return (
+  <>
+  <div className={`${location.pathname==='/store' ? `gr-${grid}` :"col-3"}`}>
     <Link className='product-card position-relative'>
         <div className='wishlist-icon position-absolute'>
             <Link><img src='images/wish.svg' alt=''/></Link>
@@ -30,6 +36,37 @@ const ProductCard = () => {
 
     </Link>
   </div>
+
+
+  <div className={`${location.pathname==='/store' ? `gr-${grid}` :"col-3"}`}>
+    <Link className='product-card position-relative'>
+        <div className='wishlist-icon position-absolute'>
+            <Link><img src='images/wish.svg' alt=''/></Link>
+        </div>
+        <div className='product-image'>
+            <img className='img-fluid' src='images/watch.jpg' alt='product image'/>
+            <img className='img-fluid' src='images/watch-01.jpeg' alt='product image'/>
+        </div>
+        <div className='product-details'>
+            <h6 className='brand'>Havels</h6>
+            <h5 className='product-title'>
+                Kids headphones bulk 10pack multi colored for students
+            </h5>
+            <ReactStars count={5} size={24} value={4}  edit={false} activeColor="#ffd700" />
+            <p className='price'>$100.00</p>
+        </div>
+        <div className='action-bar position-absolute'>
+            <div className='d-flex flex-column gap-15'>
+            <Link><img src='images/prodcompare.svg' alt='compare'/></Link>
+                <Link><img src='images/view.svg' alt='view'/></Link>
+                <Link><img src='images/add-cart.svg' alt='addcart'/></Link>
+            </div>
+        </div>
+
+    </Link>
+  </div>
+  </>
+  );
 }
 
 export default ProductCard
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -3,9 +3,9 @@ import BreadCrumb from '../components/BreadCrumb'
 import {Helmet} from 'react-helmet'
 import Meta from '../components/Meta'
 import ReactStars from 'react-rating-stars-component'
+import ProductCard from '../components/ProductCard'
 function OurStore() {
   const [grid, setGrid]=useState(4);
- alert(grid);
   return <>
   <Meta title={"Our Store"}/>
   <BreadCrumb title="Our Store"/>
@@ -160,17 +160,20 @@ function OurStore() {
             <div className='d-flex align-items-center gap-10'>
               <p className='totalproducts mb-0'>21 Products</p>
               <div className='d-flex gap-10 align-items-center grid'>
-                <img onClick={()=>{setGrid(4)}}  src='images/gr4.svg' alt='grid' className='d-block img-fluid' />
-                <img onClick={()=>{setGrid(3)}}  src='images/gr3.svg' alt='grid' className='d-block img-fluid' />
-                <img onClick={()=>{setGrid(2)}}  src='images/gr2.svg' alt='grid' className='d-block img-fluid' />
-                <img onClick={()=>{setGrid(1)}}  src='images/gr.svg' alt='grid' className='d-block img-fluid' />
+                <img onClick={()=>{setGrid(3)}}  src='images/gr4.svg' alt='grid' className='d-block img-fluid' />
+                <img onClick={()=>{setGrid(4)}}  src='images/gr3.svg' alt='grid' className='d-block img-fluid' />
+                <img onClick={()=>{setGrid(6)}}  src='images/gr2.svg' alt='grid' className='d-block img-fluid' />
+                <img onClick={()=>{setGrid(12)}}  src='images/gr.svg' alt='grid' className='d-block img-fluid' />
               </div>
 
             </div>
             </div>
           </div>
           <div className='products-list pb-5'>
+           <div className='d-flex gap-10 flex-wrap'>
+           <ProductCard grid={grid}/>
 
+           </div>
           </div>
         </div>
       </div>
